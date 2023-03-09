cf887cc0183ad504e12935b0daa902c5ec9b0045
Shira Eisen <shira.eisen@admins-MacBook-Pro.local>
2023-03-09 16:27:17 -0500

    add dependency in ticket order mapping model

--- models/ticket_order_mapping/ticket_order_mapping_intermediate/ticket_order_mapping_temp_dbt.sql
+++ models/ticket_order_mapping/ticket_order_mapping_intermediate/ticket_order_mapping_temp_dbt.sql
@@ -1,5 +1,10 @@
+with t as 
+(
+    select * from {{ ref('tickets_for_order_mapping_temp_dbt')}}
+)
+
 select  distinct cast(t.id as string) ticket_id, first_value(fo.id) over (partition by t.id order by fo.created_at) fulfillment_orders_id 
-from    `dbt_fivetran.tickets_for_order_mapping_temp_dbt` t ---------
+from    t --`dbt_fivetran.tickets_for_order_mapping_temp_dbt` t ---------
         inner join `core_prod_public.fulfillment_orders` fo on (
           trim(t.custom_order_appointment_number) = fo.order_id
           or trim(t.custom_order_appointment_number) = trim(external_order_id)
@@ -12,7 +17,7 @@ where   custom_order_appointment_number is not null
         and lower(trim(t.custom_order_appointment_number)) != 'na'
 union all
 select  cast(t.id as string) ticket_id, fo.id fulfillment_orders_id
-from    `dbt_fivetran.tickets_for_order_mapping_temp_dbt` t ------------
+from    t --`dbt_fivetran.tickets_for_order_mapping_temp_dbt` t ------------
         inner join `core_prod_public.fulfillment_orders` fo on split(split(description,'\nOrder ID: ')[safe_offset(1)],'\n')[safe_offset(0)]=order_id
 where   split(split(description,'\nOrder ID: ')[safe_offset(1)],'\n')[safe_offset(0)] is not null
         and (custom_order_appointment_number is null or lower(trim(t.custom_order_appointment_number)) in ('','na'))
