5d84bb19d21c43b605c5ff83e38c54e3219b5c5a
Shira Eisen <shira.eisen@admins-MacBook-Pro.local>
2023-06-06 13:29:45 -0400

    fivetran synced today for ticket mapping

--- models/ticket_order_mapping/ticket_order_mapping_staging/tickets_for_order_mapping_temp_dbt.sql
+++ models/ticket_order_mapping/ticket_order_mapping_staging/tickets_for_order_mapping_temp_dbt.sql
@@ -1,4 +1,4 @@
 select  id, created_at, custom_order_appointment_number, description
 from    `iron-zodiac-336013.zendesk_support.ticket`
---where   date(_fivetran_synced) >= current_date()-1 
-where created_at >= '2023-03-01'
+where   date(_fivetran_synced) >= current_date()-1 
+--where created_at >= '2023-03-01'
